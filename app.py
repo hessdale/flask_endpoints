5616ab0c88bda9c3c999762d7082a2857825e868
hessdale <hessdale96@gmail.com>
2023-06-06 19:34:30 -0600

    help

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import json
-import mariadb
-import dbcreds
 import dbhelper
 
 from flask import Flask
 app = Flask(__name__)
 
-conn = mariadb.connect(**dbcreds.conn_params)
-cursor = conn.cursor()
 
-
+@app.get('/api/clients')
+def get_all_clients():
+    results = dbhelper.run_procedure('call get_all_users',[])
+    if(type(results) == list):
+        result_Json = json.dumps(results,default=str)
+        return result_Json
+    else:
+        return 'something went wrong'
+    
 
 
 app.run(debug=True)
