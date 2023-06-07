fcc7c01e0cde669677238cf40cfdf68b75ba4556
hessdale <hessdale96@gmail.com>
2023-06-06 20:54:07 -0600

    messing around withj different api ends

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import json
 import dbhelper
-
-from flask import Flask
+from flask import Flask ,request
 app = Flask(__name__)
 
 
@@ -14,6 +13,26 @@ def get_all_clients():
     else:
         return 'something went wrong'
     
+@app.get('/api/loyal_clients')
+def get_loyal():
+    max_points = request.args.get('max_points')
+    results = dbhelper.run_procedure('call get_loyals(?)',[max_points])
+    if(type(results) == list):
+        result_Json = json.dumps(results,default=str)
+        return result_Json
+    else:
+        return 'something went wrong'
+    
+app.post('/api/clients')
+def post_client():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    results = dbhelper.run_procedure('call new_client(?,?)'[username,password])
+    if(type(results) == list):
+        result_Json = json.dumps(results,default=str)
+        return result_Json
+    else:
+        return 'something went wrong'
 
 
 app.run(debug=True)
